aff4424861289ed6c85b252f35ca94cb89d67dbc
kevin-jeeva <kevinjeev@gmail.com>
2021-07-15 15:37:31 -0300

    changed the client variable

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-  baseURL: "http://nodestaffapi-env.eba-6xxpauhn.us-east-1.elasticbeanstalk.com",
+  baseURL: "https://nbcc-staff-app.herokuapp.com",
 });
 
 export default apiClient;
